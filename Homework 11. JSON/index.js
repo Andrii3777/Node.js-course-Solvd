c285ce7b9ee15f4c02e0d23ac7225d70485c6771
Andrii Bibik <111527201+Andrii3777@users.noreply.github.com>
2024-06-05 11:37:00 +0300

    Update index.js

--- Homework 11. JSON/index.js
+++ Homework 11. JSON/index.js
@@ -9,7 +9,7 @@ const isEqual = (a1, a2) => JSON.stringify(a1) === JSON.stringify(a2);
 for (const test of tests) {
     customParseRes = myJSONParse(test);
     originalParseRes = JSON.parse(test);
-    console.log("Is str correctly parsed?:", isEqual(customParseRes, originalParseRes), '\n');
 
     console.log(myJSONParse(test));
+    console.log("Is str correctly parsed?:", isEqual(customParseRes, originalParseRes), '\n');
 }
